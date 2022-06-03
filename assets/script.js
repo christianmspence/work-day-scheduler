d834027e4e689479f7fde0f33e36d811d8dd223e
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-06-03 13:42:25 -0400

    user data saved to local storage when save button hit

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,14 @@
+
 //Show current day at top of page
 $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
+//set up click listener for current time stamp and user input data
+$(".saveBtn").on("click", function () {
+    
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+
+})
 
