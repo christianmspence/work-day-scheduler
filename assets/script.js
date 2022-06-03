65f5c0686da13e44c98aabab5b4735b9764f3063
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-06-03 13:48:58 -0400

    pull saved data from local storage

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,22 @@ $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
 //set up click listener for current time stamp and user input data
 $(".saveBtn").on("click", function () {
-    
+
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
+    //user input data and time stamp saved to local storage
     localStorage.setItem(time, text);
-
 })
+
+//loading saved data from local storage
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
